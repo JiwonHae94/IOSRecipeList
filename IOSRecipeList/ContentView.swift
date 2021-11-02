a1b5d0fbe7900541b0ea44c59a89f6152c8bd295
JiwonHae <hjiwon1994@gmail.com>
2021-11-03 03:15:38 +0900

    CWC+ IOS foundation Lesson 03 Challenge done

--- IOSRecipeList/ContentView.swift
+++ IOSRecipeList/ContentView.swift
@@ -8,24 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    var array = ["Element 1","Element 3","Element 2", "Element 4", "Element 5"]
+    var array = ["Apple","Mango","Peach", "Banana", "Lemon"]
+    @State var arrayDisplay:[String] = []
     
     var body: some View {
         NavigationView{
             
-            /**
-             * array : data type
-             * id : uniquely idenitifiable data
-             */
-            List(array, id: \.self) { arrayElement in
-                NavigationLink(
-                    destination: {
-                        Text("Destination")
-                    }, label: {
-                        Text(arrayElement)
-                    }
-                )
-            }.navigationTitle(Text("My List"))
+            VStack{
+                /**
+                 * array : data type
+                 * id : uniquely idenitifiable data
+                 */
+                List(arrayDisplay, id: \.self) { arrayElement in
+                    NavigationLink(
+                        destination: {
+                            Text("Destination")
+                        }, label: {
+                            Text(arrayElement)
+                        }
+                    )
+                }.navigationTitle(Text("My List"))
+                
+                Button{
+                    let randomIndex = Int.random(in: 0..<array.count)
+                    arrayDisplay.append(array[randomIndex])
+                } label:{
+                    Text("Random generate")
+                }
+            }
+            
         }
     }
 }
